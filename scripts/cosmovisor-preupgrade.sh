1381e7bc6cb107d0b1c7b3dc52e1e12c3b93d0f7
sybilsingleton <111304598+sybilsingleton@users.noreply.github.com>
2023-12-16 13:51:31 +0100

    Update cosmovisor-preupgrade.sh

--- scripts/cosmovisor-preupgrade.sh
+++ scripts/cosmovisor-preupgrade.sh
@@ -5,8 +5,9 @@ set -xe
 UPGRADE_NAME=$1
 UPGRADE_HEIGHT=$2 # unused
 
-rm -rf  $DAEMON_HOME/cosmovisor/upgrades/$UPGRADE_NAME || \
-  git clone --depth 1 --branch $UPGRADE_NAME https://github.com/BlitChain/blitchain.git $DAEMON_HOME/cosmovisor/upgrades/$UPGRADE_NAME && \
+rm -rf  $DAEMON_HOME/cosmovisor/upgrades/$UPGRADE_NAME
+
+git clone --depth 1 --branch $UPGRADE_NAME https://github.com/BlitChain/blitchain.git $DAEMON_HOME/cosmovisor/upgrades/$UPGRADE_NAME && \
   cd $DAEMON_HOME/cosmovisor/upgrades/$UPGRADE_NAME && 
   exec make build
 
