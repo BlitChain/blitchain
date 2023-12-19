b705f5eb6d005fe6449592376a97bf19736a512f
Sybil Singleton <sybiling420@proton.me>
2023-12-19 12:40:48 +0100

    add condition to bind to docker ports only when bltid server is started

--- scripts/cosmovisor-preupgrade-pull-docker.sh
+++ scripts/cosmovisor-preupgrade-pull-docker.sh
@@ -16,22 +16,25 @@ UPGRADE_NAME=\$1
 UPGRADE_HEIGHT=\$2 # unused
 
 # Docker image name from your docker-compose file
-IMAGE_NAME="blitchain/blitchain:$UPGRADE_NAME"
+IMAGE_NAME="blitchain/blitchain:\$UPGRADE_NAME"
 
 # Mapping the daemon home directory from host to container
-DAEMON_HOME_VOLUME="$DAEMON_HOME:/home/user/.blit"
+DAEMON_HOME_VOLUME="-v \$DAEMON_HOME:/home/user/.blit"
 
 # Collect all arguments for the blitd command
 BLITD_ARGS="\$@"
 
-# External ports from environment variables with default values
-# See: https://docs.cosmos.network/main/user/run-node/run-production#firewall
-# NOTE: bind to 0.0.0.0 instead of localhost if you are having trouble connecting
+# Initialize PORT_OPTIONS as an empty string
+PORT_OPTIONS=""
 
-BLIT_P2P_26656=\${BLIT_P2P_26656:-26656}
-BLIT_JSONRPC_26657=\${BLIT_JSONRPC_26657:-26657}
-BLIT_REST_1317=\${BLIT_REST_1317:-1317}
-BLIT_GRPC_9090=\${BLIT_GRPC_9090:-9090}
+# Determine if port mapping is needed based on \$BLITD_ARGS starting with "start"
+if (echo "\$BLITD_ARGS" | grep -q "^start"); then
+    # Assign port mappings directly to PORT_OPTIONS with default values, split into multiple lines for readability
+    PORT_OPTIONS="-p \${BLIT_P2P_26656:-26656}:26656 \\
+                  -p \${BLIT_JSONRPC_26657:-26657}:26657 \\
+                  -p \${BLIT_REST_1317:-1317}:1317 \\
+                  -p \${BLIT_GRPC_9090:-9090}:9090"
+fi
 
 # Check if script is run in TTY
 TTY_OPTION=""
@@ -43,11 +46,10 @@ fi
 exec docker run --rm \\
     -u \$(id -u):\$(id -g) \\
     \$TTY_OPTION \\
-    -v \$DAEMON_HOME_VOLUME \\
-    -p \$BLIT_P2P_26656:26656 -p \$BLIT_JSONRPC_26657:26657 -p \$BLIT_REST_1317:1317 -p \$BLIT_GRPC_9090:9090 \\
+    \$DAEMON_HOME_VOLUME \\
+    \$PORT_OPTIONS \\
     \$IMAGE_NAME \\
     ./bin/blitd \$BLITD_ARGS
 EOF
 
 chmod +x $DAEMON_HOME/cosmovisor/upgrades/$UPGRADE_NAME/bin/blitd
-
