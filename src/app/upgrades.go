c4c5f265703e72d38fc31d0084df11db60773600
sybilsingleton <111304598+sybilsingleton@users.noreply.github.com>
2024-03-20 06:45:57 +0100

    fix bug when upgrading
    Getting this same issue: https://github.com/cosmos/cosmos-sdk/issues/8503

--- src/app/upgrades.go
+++ src/app/upgrades.go
@@ -1,19 +1,33 @@
 package app
 
 import (
-	"context"
-	"strings"
+        "context"
+        "strings"
 
-	upgradetypes "cosmossdk.io/x/upgrade/types"
-	"github.com/cosmos/cosmos-sdk/types/module"
-	sdkversion "github.com/cosmos/cosmos-sdk/version"
+        storetypes "cosmossdk.io/store/types"
+        upgradetypes "cosmossdk.io/x/upgrade/types"
+        "github.com/cosmos/cosmos-sdk/types/module"
+        sdkversion "github.com/cosmos/cosmos-sdk/version"
 )
 
 func (app App) RegisterUpgradeHandlers() {
-	var version = strings.TrimSpace(sdkversion.Version)
+        var UpgradeName = strings.TrimSpace(sdkversion.Version)
 
-	app.UpgradeKeeper.SetUpgradeHandler(version, func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
-		return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
-	})
+        app.UpgradeKeeper.SetUpgradeHandler(UpgradeName, func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+
+                return app.ModuleManager.RunMigrations(ctx, app.Configurator(), fromVM)
+        })
+
+        upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
+        if err == nil {
+                if upgradeInfo.Name == UpgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
+                        storeUpgrades := storetypes.StoreUpgrades{
+                                Deleted: []string{"capability"},
+                        }
+
+                        // configure store loader that checks if version == upgradeHeight and applies store upgrades
+                        app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
+                }
+        }
 
 }
